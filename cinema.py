b150c8c496f30222f147ab4683103aa5c8f410f7
Alexal1234 <botnary08@gmail.com>
2024-09-19 23:55:21 +0300

    1

---
+++ cinema.py
@@ -0,0 +1,26 @@
+n = int(input())
+k = int(input())
+x = 1
+y = 1
+a = n
+A = n + 1
+ryad = x
+ost = k - 1
+while ost != 0:
+    if ryad % 2 != 0:
+        if a <= ost:
+            ost -= a
+            ryad += 1
+            x += 1
+        else:
+            y += ost
+            ost = 0
+    else:
+        if A <= ost:
+            ost -= A
+            ryad += 1
+            x += 1
+        else:
+            y += ost
+            ost = 0
+print(x, y)
